044059e24b13b9b940de2077013b6d8c450c574d
aneela-codes <aneelareiko@gmail.com>
2021-08-29 09:28:11 -0700

    adding programs details

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,26 @@ window.addEventListener('scroll', function() {
 
   console.log(scrollpos)
 })
+
+// --------------------------------PANELS------------------
+let panelButtons = document.querySelectorAll(".button_container button")
+let panelTabs = document.querySelectorAll(".tabContainer .panel")
+
+  console.log(panelTabs);
+const showPanels = (index) => {
+  let colorCode = "teal"
+  panelButtons.forEach( (node) => {
+    node.style.backgroundColor = ""
+    node.style.color=""
+  })
+  panelTabs.forEach((node) => {
+    node.style.display="none"
+  })
+  panelButtons[index].style.backgroundColor = colorCode;
+  panelButtons[index].style.color = "white"
+  panelTabs[index].style.display= "block"
+  // panelTabs[index].style.backgroundColor = colorCode;
+  // panelTabs[index].style.color = white;
+}
+
+showPanels(0)
